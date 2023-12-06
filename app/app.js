0113ea9850c9df885e69f53cd188a8ea55f06227
ehigies <ehigies@roehampton.ac.uk>
2023-12-06 20:02:34 +0000

    commit

--- app/app.js
+++ app/app.js
@@ -2,30 +2,69 @@
 
 // Import express.js
 const express = require("express");
-
+const bodyParser = require('body-parser');
 // Create express app
 var app = express();
 
 // Add static files location
 app.use(express.static("static"));
 
+app.use(bodyParser.urlencoded({ extended: true }));
 //use pug templating engine
 app.set('view engine', 'pug');
 app.set('views', './app/views');
+app.set('stylesheet', '/css');
 
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
-// Create a route for root - /
+// Create a route for home - /
 app.get("/", function(req, res) {
-    res.render("about", {'headingAbout':'About Us'});
-    
+   res.render("index"); 
 });
 
+//create a root for events
+app.get("/events", function(req, res) {
+    res.render("events"); 
+ });
+
+ // about page
+ app.get("/about", function(req, res){
+    res.render("about");
+ });
+
+ // rooms and dining page
+ app.get("/roomsanddining", function(req, res){
+    res.render("roomsanddining");
+ });
+
+ // reservation page
+ app.get('/reservation', (req, res) => {
+    res.render('reservation');
+  });
+ 
+// menu page
+ app.get('/menu', (req, res) => {
+    res.render('menu');
+  });
+
+ // Handling table booking form submission
+app.post('/book-table', (req, res) => {
+  const { tableType, numPeople, date, cust_name, cust_email, message } = req.body;
+  // Handle table booking data
+  res.send(`Table booked: Type - ${tableType}, People - ${numPeople}, Date - ${date}, cust_name - ${cust_name}, Email - ${cust_email}, message - ${message}`);
+});
+
+// Handling room booking form submission
+app.post('/book-room', (req, res) => {
+  const { roomType, numNights, checkin, cust_name, cust_email, message } = req.body;
+  // Handle room booking data
+  res.send(`Room booked: Type - ${roomType}, Nights - ${numNights}, Check-in - ${checkin}, cust_name - ${cust_name}, Email - ${cust_email}, message - ${message}`);
+});
 // Create a route for testing the db
-app.get("/db_test", function(req, res) {
+app.get("/hotel_restaurants", function(req, res) {
     // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
+    sql = 'select * from hotel_restaurants';
     db.query(sql).then(results => {
         console.log(results);
         res.send(results)
